903ba896ae6314d2a3d22c9396453dfd3c04b138
lpmi-13 <leskis@gmail.com>
2018-04-29 23:04:03 +0100

    add simple script for finding mention of 'gem'

--- scrape.rb
+++ scrape.rb
@@ -1,64 +1,25 @@
 require 'nokogiri'
 require 'httparty'
-#require 'watir'
-
-#Selenium::WebDriver::Chrome.driver_path = '/usr/lib/chromium-browser/chromedriver'
-
-def get_all_links_as_hash(page)
-  parsed_page = Nokogiri::HTML(page)
-  links = parsed_page.css('a')
-
-  all_links = Hash[links.xpath('//a[@href]').map {|link| [link.text.strip, link['href']]}]
-
-  return all_links
-end
-
-def extract_github_from_hash(hashed_links)
-  result = {}
-  hashed_links.each do |key, value|
-    if key.downcase.include?('github') and value.downcase.include?('/github')
-      result[key] = value
-    end
-  end
-
-  return result
-end
 
 puts 'what is the URL of the tutorial?'
 url = gets.strip
 
 response = HTTParty.get(url)
 
-document_links = get_all_links_as_hash(response.body)
+def find_code_in_page(page)
+  # this is where we search for 'gem'
+  lines = page.lines
 
-filtered_links = extract_github_from_hash(document_links)
+  results_array = []
 
-puts filtered_links
+  lines.each do |line|
+    if line.include?('gem')
+      results_array.push(line)
+    end
+  end
 
-#def find_all_pages(url)
+  return results_array
+end
 
-#  browser = Watir::Browser.new :chrome, headless: true
-  # starts at the first page of the tutorial
-#  browser.goto(url)
-  # saves the url of the first page
-#  current_url = browser.url
-
-  # grab the page content here for later, then go to the next
-  # page if it exists
-
-#  page_content = browser.html
-
-#  js_link = browser.link(:class, "rightArrow")
-#  js_link.click if js_link
-
-#  browser.wait_until { browser.url != current_url }
-
-#  page_content += browser.html
-
-#  open('content.txt', 'w') { |f|
-#    f.puts page_content
-#  }
-
-#end
-
-#find_all_pages(url)
+gem_stuff = find_code_in_page(response.body)
+puts gem_stuff
